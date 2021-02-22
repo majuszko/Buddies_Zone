be699dbef07d7ef10ef131b3cd109f93972b67da
majuszko <majamrozinska97@gmail.com>
2021-02-22 13:25:38 +0100

    Fixed view

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,27 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject Pref;
     public GameObject Canv;
-    public GameObject Cam;
+    public GameObject player;
+    [SerializeField] private CinemachineFreeLook playerCamera = null;
 
 
     public void SpawnPlayer()
     {
         float randx = Random.Range(80f, 90f);
         float randz = Random.Range(-70f, -80f);
-        PhotonNetwork.Instantiate(Pref.name,
+        player = PhotonNetwork.Instantiate(Pref.name,
             new Vector3(randx, 2.88f, randz),
             Quaternion.identity, 0);
         
         
         
         Canv.SetActive(false);
-        
-        
         
         //Cam.SetActive(false);
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class Player : Photon.MonoBehaviour
 {
@@ -15,32 +16,31 @@ public class Player : Photon.MonoBehaviour
     public float smooth = 0.1f;
     private float smoothVelocity;
     public Transform cam;
+    private GameManager gm;
+    [SerializeField] private CinemachineFreeLook playerCamera;
 
     private void Awake()
     {
         if (pV.isMine)
         {
             playerCam.SetActive(true);
-            cam = GameObject.FindWithTag("MainCamera").gameObject.transform;
-            
         }
-        //cam = GameObject.FindWithTag("MainCamera").transform;
+        
     }
 
     void Start()
     {
         if (pV.isMine) {
-            Update(); }
-        
+            cam = GameObject.FindWithTag("MainCamera").gameObject.transform;
+        }
         
     }
-
     
     void Update()
     {
 
-        //if (pV.isMine)
-        //{
+        if (pV.isMine)
+        {
 
             float hor = Input.GetAxisRaw("Horizontal");
             float ver = Input.GetAxisRaw("Vertical");
@@ -54,7 +54,10 @@ public class Player : Photon.MonoBehaviour
                 Vector3 moveDir = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
                 control.Move(moveDir.normalized * speed * Time.deltaTime);
             }
-        //}
+            
+            playerCamera.Follow = gm.player.transform;
+            playerCamera.LookAt = gm.player.transform;
+        }
     }
         
 }
