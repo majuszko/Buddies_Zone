3d7ab06d016da1b131305dffdb3d97943408609a
majuszko <majamrozinska97@gmail.com>
2021-02-22 19:31:37 +0100

    Added nametags

---
+++ Assets/UsernameDirection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsernameDirection : MonoBehaviour
+{
+    private Transform cam;
+    
+    void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position+cam.rotation*Vector3.forward, cam.rotation*Vector3.up);
+    }
+}
